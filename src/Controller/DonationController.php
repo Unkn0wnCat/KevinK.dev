25ebf70ac0a611275d207cd834906b465cfcdeea
Kevin <kevin@1in9.net>
2019-07-20 22:16:21 +0200

    Updated Payment Module to support Stripe.

--- src/Controller/DonationController.php
+++ src/Controller/DonationController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Class DonationController
@@ -28,6 +32,35 @@ class DonationController extends AbstractController
     {
         return $this->render('donation/donationComplete.html.twig', [
             'module' => 'donation',
+        ]);
+    }
+
+    /**
+     * @Route("/donate/stripe/{type}/{amount}", defaults={"currency"="EUR"}, requirements={"currency"="EUR|USD", "amount"="\d+", "type"="card|sofort|giropay"}, name="donationStripe")
+     * @param int $amount
+     * @param string $currency
+     * @param string $type
+     * @return Response
+     */
+    public function stripeDonation(int $amount, string $currency, string $type) {
+        Stripe::setApiKey($_ENV["STRIPE_PRIVATE_KEY"]);
+
+        $session = Session::create([
+            'payment_method_types' => [$type],
+            'line_items' => [[
+                'name' => 'Donation to KevinK.dev',
+                'description' => 'Donation to Kevin Kandlbinder',
+                'amount' => $amount,
+                'currency' => $currency,
+                'quantity' => 1,
+            ]],
+            'success_url' => $this->generateUrl("donationThanks", [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl("donation", [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+        return $this->render("donation/donateStripe.html.twig", [
+            "CHECKOUT_SESSION_ID" => $session->id,
+            "stripe_public_key" => $_ENV["STRIPE_PUBLIC_KEY"]
         ]);
     }
 }
